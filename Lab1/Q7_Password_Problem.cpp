02f60813682e4d9bc6b9b04fcdf9d0cc9411633a
Johanna <johanna.hassinen01@gmail.com>
2025-03-30 16:31:59 +0300

    Lab 1 Question 7 kesken

---
+++ Lab1/Q7_Password_Problem.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define MIN_LENGTH 8 //>= 8 characters
+
+int main()
+{
+    string password;
+
+    cout << "Enter a password" << endl;
+
+    cin >> password;
+
+    int length = password.length();
+
+    if (length > MIN_LENGTH)
+    {
+        cout << "Strong Password" << endl;
+    }
+    
+    else
+    {
+        cout << "Weak Password \n -Too short" << endl;
+    }
+
+
+    
+    /*FOR LOOP Iterate through the string to check the presence of 
+    required character types*/
+
+    //for ()
+    {
+        //One uppercase letter
+        //One lowercase letter
+        //One digit
+        //One special character (e.g., !@#$%^&*)
+
+        /*POHDINTA:
+        - mites ois boolean käyttäminen to check if true or false?*/
+    }
+
+    return 0;
+}
