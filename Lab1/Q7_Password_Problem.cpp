2d926af93c6557b78ae57d58d537ca99b2255eff
Johanna <johanna.hassinen01@gmail.com>
2025-04-02 00:31:28 +0300

    Lab 1 Q7 password problem, finished!

--- Lab1/Q7_Password_Problem.cpp
+++ Lab1/Q7_Password_Problem.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
 #include <string>
+#define MIN_LENGTH 8 // >= 8 characters
 
 using namespace std;
 
-#define MIN_LENGTH 8 // >= 8 characters
-
 int main()
 {
-    string password;
-    int uppercase = 0;
-    int lowercase = 0;
-    int digit = 0;
-    int special_character = 0;
+    string password; // Defining variables
+    bool uppercase = false;
+    bool lowercase = false;
+    bool digit = false;
+    bool special_character = false;
 
-    cout << "Enter a password" << endl;
-    cin >> password;
+    cout << "Enter a password" << endl; // Printing instructions for the user
+    cin >> password; // Getting the password from the user
 
-    int length = password.length();
-
-    //IDK KOODINPÄTKIÄ, IDK JÄRKEVÄÄ JÄRJESTYSTÄ
+    int length = password.length(); // Getting the length of the password in order to check each character of the password
 
     // CODE TO CHECK IF THE PASSWORD HAS SPECIFIC CHARACTER TYPES
-    for (int i = 0; password[i]; i++)
+    for (int i = 0; password[i]; i++) // Going through each character of the password
     {
+        // CODE TO CHECK IF THE PASSWORD HAS UPPERCASE LETTERS
         if (password[i] == 'A' || password[i] == 'B' || password[i] == 'C' || password[i] == 'D' 
             || password[i] == 'E' || password[i] == 'F' || password[i] == 'G' || password[i] == 'H' 
             || password[i] == 'I' || password[i] == 'J' || password[i] == 'K' || password[i] == 'L'
@@ -31,7 +29,7 @@ int main()
             || password[i] == 'U' || password[i] == 'V' || password[i] == 'W' || password[i] == 'X'
             || password[i] == 'Y'|| password[i] == 'Z')
         {
-            uppercase++; //The no. of vowels is increased by 1 if password[i] is a vowel
+            uppercase = true;
         }   
         
         // CODE TO CHECK IF THE PASSWORD HAS LOWERCASE LETTERS
@@ -43,38 +41,59 @@ int main()
             || password[i] == 'u' || password[i] == 'v' || password[i] == 'w' || password[i] == 'x'
             || password[i] == 'y'|| password[i] == 'z')
         {
-            lowercase++; //The no. of vowels is increased by 1 if password[i] is a vowel
+            lowercase = true;
         }
 
+        // CODE TO CHECK IF THE PASSWORD HAS DIGITS
         if (password[i] == '0' || password[i] == '1' || password[i] == '2' || password[i] == '3' 
             || password[i] == '4' || password[i] == '5' || password[i] == '6' || password[i] == '7' 
             || password[i] == '8' || password[i] == '9')
         {
-            digit++; //The no. of digits is increased by 1 if password[i] is a digit
+            digit = true;
         }
 
+        // CODE TO CHECK IF THE PASSWORD HAS SPECIAL CHARACTERS
         if (password[i] == '!' || password[i] == '@' || password[i] == '#' || password[i] == '$' 
             || password[i] == '%' || password[i] == '^' || password[i] == '&' || password[i] == '*')
         {
-            special_character++; //The no. of digits is increased by 1 if password[i] is a digit
+            special_character = true;
         }
-
     }
 
-
-
-    //TEE JOKAINEN IF JUTTU SISÄKKÄIN????
-    if (length > MIN_LENGTH && uppercase >= 1 && lowercase >= 1
-        && digit >= 1 && special_character >= 1) // Checking conditions one by one
+    if (length > MIN_LENGTH && uppercase == true && lowercase == true
+        && digit == true && special_character == true) // If all conditions are met, printing "Strong Password"
         {
             cout << "Strong Password" << endl;
         }
-    else
+    else // If conditions aren't met for a strong password, printing out "Weak Password" with the missing criteria
         {
-            cout << "Weak Password \n -Too short" << endl;
+            cout << "Weak Password" << endl;
+
+            if (length < MIN_LENGTH)
+                {
+                    cout << "-Too short" << endl;
+                }
+
+            if (uppercase == false)
+                {
+                    cout << "-You should have at least 1 uppercase letter" << endl;
+                }
+            
+            if (lowercase == false)
+                {
+                    cout << "-You should have at least 1 lowercase letter" << endl;
+                }
+
+            if (digit == false)
+                {
+                    cout << "-You should have at least 1 digit" << endl;
+                }
+
+            if (special_character == false)
+                {
+                    cout << "-You should have at least 1 special character" << endl;
+                }
         }
-
-//jos true, printtaa x, jos false, print y
-
+    
     return 0;
 }
