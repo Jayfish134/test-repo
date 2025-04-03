06e7c2387408ac88b73df43f973de929c94a7ab0
Johanna <johanna.hassinen01@gmail.com>
2025-04-03 20:39:07 +0300

    Lab2 E5, updated the commenting and made the output neater

--- Lab2/E5.cpp
+++ Lab2/E5.cpp
@@ -9,18 +9,27 @@ int main()
 
     int array2[n]; // The soon-to-be "reversed" array of array1
     
-    int* arrayptr = array1 + n - 1; // Creating a pointer to point at the last element of array1
+    int* arrayptr = &array1[0] + n - 1; // Creating a pointer to store the address of the last element of array1
 
     for (int i = 0; i < n; i++) // Transversal of array1
     {
         /*array2[0] will get the last value in array1, so array2[0] = 15, array2[1] = 10, etc.*/
-        array2[i] = *arrayptr;
+        array2[i] = *arrayptr; // Assigning the value stored in the pointer as the value of the element of array2[i]
         arrayptr--; // Moving the pointer backwards
     }
 
     for (int i = 0; i < n; i++) 
-    { 
-        cout << array2[i] << ", "; // Printing out the new array element by element
+    {
+        if(i == n - 1)
+        {
+            cout << array2[i];
+            /*Code to neaten up the output. If the last element is being printed out, 
+            there will be no comma printed out at the end of the last element*/
+        }
+        else
+        {
+            cout << array2[i] << ", "; // Printing out the new array element by element
+        }
     }
 
     cout << endl; // Printing an empty line for aesthetic purposes regarding the terminal
