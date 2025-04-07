835246d71f7d964be21a0f5eb9667713276bc2c8
Johanna <johanna.hassinen01@gmail.com>
2025-04-07 15:58:41 +0300

    Lab2 task 1 coding done, comments are missing

--- Lab2/E1.cpp
+++ Lab2/E1.cpp
@@ -1,59 +1,41 @@
 #include <iostream>
-
 using namespace std;
 
-// EDIT A AND B TO BE INTEGER AND BOOL
-
-int multiply(int a, int b) // Function that returns the product of a and b
+int intMultiply(int a, int b) // Function that returns the product of two integers
 {
     return a * b;
 }
 
-double multiply(double a, double b)
+double doubleMultiply(double c, double d) // Function that returns the product of two double numbers
 {
-    return a * b;
+    return c * d;
 }
 
-idk multiply(int a, double b, bool flag)
+int threeInputMultiply(int e, double f, bool flag) // Function that returns the product of an integer and a double with "optional" rounding of the number
 {
-    if(flag == false)
+    if(flag == false) // If the value of the flag is false, the product is returned as a double
     {
-        double result = integer * double;
-        return result;
+        double falseProduct = e * f;
+        return falseProduct;
     } 
-
-    if(flag == true)
+    else // If the value of the flag is true, the product is returned as an integer that is rounded either up or down depending on its value
     {
-        double result = integer * double;
-        
-        //pitää tarkastaa onko suurempi kuin 0.5 vai ei, sen perusteella floor() tai ceil()
-        if(result >) // yritä kirjottaa ehto pyöristämiselle
-        return // result should be ceiled or floored? what does that mean
-        
-        /*floor function returns the integer value 
-        just lesser than the given rational value. 
-        ceil function returns the integer value just 
-        greater than the given rational value*/
+        double product = e * f + 0.5;
+        int roundedProduct = (int) product; // e.g. if e*f = 4.999, it will return 5 because of e*f+0.5 = over 5. If e*f=4.111, it will return 4, so flooring and ceiling is done!
+        return roundedProduct;
     }
 }
 
-
 int main()
 {
-    int integer_product = multiply(1, 2);
-    double double_product = multiply (1.2, 1.2);
-    
-    cout << "The product integers is " << integer_product << endl; // Printing out the products to the user by calling the function
-    cout << "The product of doubles is " << double_product << endl;
+    int a = 2, b = 8;
+    double c = 1.22, d = 2.48;
+    int e = 3; double f = 5.24;
+    bool flag = true;
 
+    cout << "The product of " << a << " and " << b << " is " << intMultiply(a, b) << endl; // Printing out the products to the user by calling the function
+    cout << "The product of " << c << " and " << d << " is " << doubleMultiply(c, d) << endl;
+    cout << "The product of " << e << " and " << f << " is " << threeInputMultiply(e, f, flag) << endl;
 
-    /* HUOM. 
-        The main will call the multiply functions with
-        different inputs. Print out the final result 
-        values in different cases*/
     return 0;
 }
-
-/*I would have taken values a and b and c from the user, but
-when I tried to input double values, it gave the product to me
-as a value of 0 instead of e.g. 1.44 for some reason.*/
