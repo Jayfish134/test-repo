c0ffd0d63bfb6275a1bd6969c99616248100c071
Johanna <johanna.hassinen01@gmail.com>
2025-04-27 12:55:17 +0300

    Lab 4 A4 now with the cin.ignore(), so the code works just as intended

--- Lab4/A4_cin.cpp
+++ Lab4/A4_cin.cpp
@@ -9,13 +9,16 @@ int main()
 
     cout << "Enter your age: ";
     cin >> age;
+    cin.ignore(); // Clears the leftover newline
 
-    cout << "Enter your full name: ";
-    getline(cin, name); // This will be skipped!
-    /*cin.ignore() is used to discard characters from the input buffer. 
+    /*
+    cin.ignore() is used to discard characters from the input buffer. 
     This is especially helpful after using cin >>, which leaves a newline 
     character (\n) in the input stream.
     */
+
+    cout << "Enter your full name: ";
+    getline(cin, name); // Works correctly now, because of the cin.ignore()
 
     cout << "Age: " << age << ", Name: " << name << endl;
 }
